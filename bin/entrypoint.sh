6f0bea3ab2879cfa3762a8ea311e7c6866171b2e
Tobias Tiger <34452589+tobiastiger@users.noreply.github.com>
2019-11-06 12:21:40 +0100

    Fix permission issue for HOME in container

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [ $(id -u) -ne 0 ]; then
   # If we don't start as root, we continue
   exec "$@"
@@ -11,10 +10,11 @@ else
   group_id=${local_group_id:-9001}
   user_name=${USER:-user}
   group_name=${local_group_name:-user}
+  user_home=${HOME:-/home}
 
-  
+
   groupadd -f -g $group_id $group_name
-  
+
 
   # Create all groups the user belongs to
   group_names=(${local_group_names//,/ })
@@ -24,7 +24,7 @@ else
     groupadd -f -g $group_id ${group_names[ $index ]}
     (( index++ ))
   done
-  
+
 
   # Create and add any extra groups
   if [ ! -z "$local_group_names" ]; then
@@ -37,14 +37,19 @@ else
   # Allow user to use sudo without password
   echo "$user_name ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$user_name
   chmod 0440 /etc/sudoers.d/$user_name
-  chown $user_name $HOME
+
+
+  chown $user_name:$group_name $user_home
+
 
   if [ -e /tmp/$user_name ]; then
     chown -R $user_name /tmp/$user_name
   fi
 
+
   # Ability to run Salt as user
   chown -R $user_name /etc/salt /var/cache/salt /var/log/salt /var/run/salt
+
 
   # Create a file in /tmp to use for synchronization check
   touch /tmp/initializationDone
@@ -54,7 +59,7 @@ else
   if [[ "$@" != "" ]]; then
     exec sudo -H -E -u $user_name "$@"
   else
-    exec sudo -H -E -u "$user_name" bash
+    exec sudo -H -E -u $user_name bash
   fi
 
 fi
