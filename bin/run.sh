95e55dd341828e5a93f39d80af5944eabce10cbb
Tobias Tiger <34452589+tobiastiger@users.noreply.github.com>
2019-11-14 16:13:07 +0100

    Permission fix in run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -3,17 +3,24 @@
 set -eu
 set -o pipefail
 
+if [ $(id -u) -ne 0 ]; then
+  echo $'Please run as root. Exiting.\n'
+  exit
+fi
 
-user=$USER
+user=$SUDO_USER
+group=$(id -gn $user)
 homedir=$HOME
 statesdir=$(pwd)/states
 os=$(uname)
 
+chown -R $user:$group /var/log/salt /var/cache/salt /var/run/salt /etc/salt
 
 # Set up grains if they don't exist
 if [ ! -f grains ]; then
   salt-call --config=./ grains.setvals "{ \
     \"user\": \"${user}\", \
+    \"group\": \"${group}\", \
     \"homedir\": \"${homedir}\", \
     \"statesdir\": \"${statesdir}\", \
     \"os\": \"${os}\"
